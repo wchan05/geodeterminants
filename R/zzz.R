2d53b3820c32b8b5b31e546bb736c1be437474d2
wchan05 <wesleychan05@gmail.com>
2025-08-15 17:13:59 -0400

    Initial commit

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.onAttach <- function(libname, pkgname)
+  {
+    packageStartupMessage("
+    Thank you for loading geodeterminants!
+    Use `?geodeterminants` to see available functions and information.
+    These packages must be installed for proper functionality:
+    **tidyverse**, **tidygeocoder**, **tibble**, **tidycensus**(API KEY),
+    **tigris**, **sf**, **RAQSAPI**(API KEY), **readxl**")
+  }
