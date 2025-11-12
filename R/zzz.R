85fa86c5c5220284b00555a3f0f0f932ffdaea15
wchan05 <wesleychan05@gmail.com>
2025-11-11 23:38:34 -0500

    structured zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,21 @@
 .onAttach <- function(libname, pkgname)
   {
     packageStartupMessage("
-    Thank you for loading geodeterminants!
-    Use `?geodeterminants` to see available functions and information.
-    These packages must be installed for proper functionality:
-    **tidyverse**, **tidygeocoder**, **tibble**, **tidycensus**(API KEY),
-    **tigris**, **sf**, **readxl**.
-    ***You MUST download csv and excel files from [Github](https://wchan05.github.io/data_access/).***")
-  }
+Thank you for loading geodeterminants!
+Use `?geodeterminants` to see available functions and information.
+
+These packages must be installed for proper functionality (use `install.packages(\"package_name\")`):
+- tidyverse
+- tidygeocoder
+- tibble
+- tidycensus (Individual API key required)
+- tigris
+- sf
+- readxl
+
+Load with packages with: library(package_name)
+
+***You MUST download csv and excel files from:***
+https://wchan05.github.io/data_access/
+")
+}
